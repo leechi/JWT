f6b73b164a70bdc9ebf30bfc44c02a60dbe6ad37
이치연 <leechi@ichiyeons-MacBook-Air.local>
2023-11-12 21:46:02 +0900

    feat(login): Add client code

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Login from './components/Login';
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
