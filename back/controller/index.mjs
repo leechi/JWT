c4f597d9c8337b6fd9cd6dc6415a721b62069ca2
이치연 <leechi@ichiyeons-MacBook-Air.local>
2023-11-12 23:39:29 +0900

    feat:Add login success

--- back/controller/index.mjs
+++ back/controller/index.mjs
@@ -46,21 +46,57 @@ const login = (req,res, next)=>{
 }
 
 const accessToken = (req,res)=>{
-    
+    const token = req.cookies.accessToken
+    const data = jwt.verify(token, process.env.ACCESS_SECRET)
+    const userData = db.filter(item => {
+        return item.email === data.email
+    })[0]
+    const {password, ...others} = userData;
+
+
+    res.status(200).json(others)
 }
 
 const refreshToken = (req,res)=>{
+    // 용도 : access token을 갱신.
+    const token = req.cookies.refreshToken;
+    const data = jwt.verify(token, process.env.REFRECH_SECRET)
+    const userData = db.filter(item=>{
+      return item.email === data.email;
+    })[0]
 
+    // access Token 새로 발급
+    const accessToken = jwt.sign({
+      id : userData.id,
+      username : userData.username,
+      email : userData.email,
+    }, process.env.ACCESS_SECRET, {
+      expiresIn : '1m',
+      issuer : 'leechi',
+    });
+    res.cookie("accessToken", accessToken, {
+        secure : false, // 현재 http를 사용중이라서 false
+        httpOnly : true,
+    })
+
+    res.status(200).json("Access Token Recreated")
 }
 
 
 const loginSuccess = (req,res)=>{
+    const token = req.cookies.accessToken
+    const data = jwt.verify(token, process.env.ACCESS_SECRET)
+    const userData = db.filter(item =>{
+        return item.email === data.email
+    })[0]
 
+    res.status(200).json(userData)
 }
 
 
 const logout = (req,res)=>{
-
+    res.cookie('accessToken', '');
+    res.status(200).json('Logout Success')
 }
 
 export {login, loginSuccess, logout, accessToken, refreshToken}
